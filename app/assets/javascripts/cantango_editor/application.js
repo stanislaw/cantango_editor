54c5ab5eae644887bd59bc7e4ed59cb0c677c6d3
stanislaw <s.pankevich@gmail.com>
2011-10-25 17:25:42 +0300

    choosing better direction

--- app/assets/javascripts/cantango_editor/application.js
+++ app/assets/javascripts/cantango_editor/application.js
@@ -9,7 +9,7 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $('.new_target_link').live('click', function(event){
+  $('.new_target_link:not([disabled=disabled])').live('click', function(event){
     // event.preventDefault(); 
     $(this).addClass("link_processing");
     return false;
@@ -17,10 +17,21 @@ $(document).ready(function() {
   
   $('.delete_target_link').live('click', function(event){
     $(this).prev().removeAttr('disabled');
+    $('form').submit();
     return false;
   });
 
+  $('.models_cell').hover( 
+    function(event){ 
+      $('.new_target_link').css('visibility', 'hidden');     
+      $(this).children('.new_target_link').css('visibility', 'visible');
+    },
+    function(event){ 
+      $(this).children('.new_target_link').css('visibility', 'hidden');
+    }
+  );
+
   $('.models_select').live('ajaxLoad', function(){
-    $(this).selectmenu({style:'popup', width: 220, maxHeight:200});
+    $(this).selectmenu({style:'popup', width: 220, maxHeight:200, change: function(e, object){$('form').submit();}});
   });
 });
