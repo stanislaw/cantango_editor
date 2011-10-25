5c915a56a976db9906d262b3dad6149d9f35775d
stanislaw <s.pankevich@gmail.com>
2011-10-26 00:06:59 +0300

    refactor permissions_controller

--- app/controllers/cantango_editor/permissions_controller.rb
+++ app/controllers/cantango_editor/permissions_controller.rb
@@ -1,6 +1,8 @@
 module CantangoEditor
   class PermissionsController < ApplicationController 
-   
+  
+    before_filter :process_params, :only => [:new_target, :new_category, :new_custom_target]
+
     def index
       @actions = Permissions.actions_available     
       @models = Permissions.models_available
@@ -16,43 +18,26 @@ module CantangoEditor
 
     def update 
       Permissions.update_permissions! params
-
+      
       index
     end
 
 
-    def new_custom_target
+    def process_params
       [:ptype, :pgroup, :can_action, :crud_action].each do |param|
         instance_variable_set("@#{param}", params[param])
       end
+    end
 
-      respond_to do |format|
-        format.js
-      end
+    def new_custom_target
     end
     
     def new_target
       @new_targets_available = Permissions.models_available_names - params[:existing_targets].to_a
-     
-      [:ptype, :pgroup, :can_action, :crud_action].each do |param|
-        instance_variable_set("@#{param}", params[param])
-      end
-
-      respond_to do |format|
-        format.js
-      end
     end
 
     def new_category
       @new_categories_available = Permissions.categories_available - params[:existing_targets].to_a
-     
-      [:ptype, :pgroup, :can_action, :crud_action].each do |param|
-        instance_variable_set("@#{param}", params[param])
-      end
-
-      respond_to do |format|
-        format.js
-      end
     end
     
     def preview_raw
