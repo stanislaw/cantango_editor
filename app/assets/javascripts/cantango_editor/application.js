7d9ac0836ba64d04a69c9311bae7b0717c81045f
stanislaw <s.pankevich@gmail.com>
2011-10-25 18:34:36 +0300

    almost ready for styling

--- app/assets/javascripts/cantango_editor/application.js
+++ app/assets/javascripts/cantango_editor/application.js
@@ -9,6 +9,8 @@
 //= require_tree .
 
 $(document).ready(function() {
+
+  // Fetch Models select dialog
   $('.new_target_link:not([disabled=disabled])').live('click', function(event){
     // event.preventDefault(); 
     $(this).addClass("link_processing");
@@ -17,27 +19,30 @@ $(document).ready(function() {
 
     return false;
   });
-  
+ 
+  // Delete one Model
   $('.delete_target_link').live('click', function(event){
     $(this).prev().removeAttr('disabled');
     $('form').submit();
     return false;
   });
 
-  $('.models_cell').hover( 
-    function(event){ 
-      $('.new_target_link').css('visibility', 'hidden');     
-      $(this).children('.new_target_link').css('visibility', 'visible');
-    },
-    function(event){ 
-      $(this).children('.new_target_link').css('visibility', 'hidden');
-    }
-  );
+  // Hovers events for table cells
+  $('.models_cell').live('mouseenter', function(event){
+    $('.new_target_link').css('visibility', 'hidden');     
+    $(this).children('.new_target_link').css('visibility', 'visible');
+  });
 
+  $('.models_cell').live('mouseleave', function(event){ 
+    $(this).children('.new_target_link').css('visibility', 'hidden');
+  });
+  
+  // Enable selectmenu() on <select>
   $('.models_select').live('ajaxLoad', function(){
     $(this).selectmenu({style:'popup', width: 220, maxHeight:200, change: function(e, object){$('form').submit();}});
   });
 
+  // Closes Models select dialog
   $('.models_select_close_link').live('click', function(){
     $('.new_target_link').removeAttr('disabled');
     $(this).parents('div').remove();
