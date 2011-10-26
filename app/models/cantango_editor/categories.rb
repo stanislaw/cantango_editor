ec8e217024559c3ed8f0275ab7305492f8e5dcfc
stanislaw <s.pankevich@gmail.com>
2011-10-26 03:05:18 +0300

    categories.yml

--- app/models/cantango_editor/categories.rb
+++ app/models/cantango_editor/categories.rb
@@ -13,7 +13,36 @@ module CantangoEditor
     end
   
     def available
-      ['^improvisation_music', '^complex_music', '^texts'].map{|c| Category.new(c) }
+      yml_file_content.keys.map{|c| Category.new(c) }
     end
+
+    def yml_file_content
+      yml_content = YAML.load_file(categories_file)
+      validate_content yml_content
+
+      yml_content
+    end
+
+    def validate_content content
+    end
+
+    def categories_file
+      create_empty_categories_file if !File.file? categories_file_path
+
+      categories_file_path
+    end
+
+    def create_empty_categories_file
+      File.open(categories_file_path, 'w') do |file|
+        YAML.dump({}, file)
+      end
+
+      categories_file_path
+    end
+    
+    def categories_file_path
+      File.join Rails.root + "config/" + "categories.yml"
+    end
+
   end
 end
